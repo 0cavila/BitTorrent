32d8f8e00342f8e440b4f7ac16ca00a56b502e51
0cavila <42749704+0cavila@users.noreply.github.com>
2018-11-28 14:02:48 -0600

    Add files via upload

--- tracker.js
+++ tracker.js
@@ -7,6 +7,12 @@ function Tracker(){
   //implement all six functions below
 
 
-
 function numOfSeeds(List, Length){
   let found = 0;
+    for(let i = 0; i < list.length; i++){
+      if(list[i] === value){
+        found++;
+      }
+
+    }
+      return found
