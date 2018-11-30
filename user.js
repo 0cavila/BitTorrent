88b417c09fc424957d70f390430b42e944c34afa
0cavila <42749704+0cavila@users.noreply.github.com>
2018-11-30 13:59:43 -0600

    Add files via upload

--- user.js
+++ user.js
@@ -4,9 +4,35 @@ function User(tracker, torrent){
   let myTracker = tracker;
   let myTorrent = torrent;
 
+let state = "seeds"
+
+let Downloadsped= 0;
+
+let uploadspeed= 0;
+
+
   //Type all of your code in here.
-
-
-
+function startDownload(){
 
 }
+function updareTracker(){
+  function Tracker(){
+    function average(list){
+      let sum= 0;
+      for(let i < list.length; i++){
+        sum = sum + list [i];
+      }
+      return sum / list.length
+    }
+  }
+}
+function checkProgress (){
+  function TorrentFile(){
+    function average (list){
+      let sum 0;
+      for(let i = 0 < list.length; i++){
+        sum = sum + list[i];
+      }
+      return sum / list.length; 
+    }
+    
